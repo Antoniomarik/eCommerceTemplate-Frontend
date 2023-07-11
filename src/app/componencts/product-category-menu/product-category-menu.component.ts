44778d81a65ee928e30dd15731d03dd011b6d6e2
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-12 00:19:56 +0200

    added service to fetch categories from database and display them in new component (previous - hardcoded)(create new component and class)

---
+++ src/app/componencts/product-category-menu/product-category-menu.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {ProductCategory} from "../../common/product-category";
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product-category-menu',
+  templateUrl: './product-category-menu.component.html',
+  styleUrls: ['./product-category-menu.component.css']
+})
+export class ProductCategoryMenuComponent {
+  productCategories: ProductCategory[] = [];
+
+  constructor(private productService: ProductService) {
+    this.listProductCategories();
+  }
+
+  private listProductCategories() {
+    this.productService.getProductCategories().subscribe(
+      data => {
+        this.productCategories = data;
+      }
+    )
+  }
+
+  protected readonly ProductCategory = ProductCategory;
+}
