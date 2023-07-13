54459a1b318d8d4aa15fa819dd29b44e9fe6567d
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-14 00:06:36 +0200

    added new method for paginating searched producs

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -36,6 +36,17 @@ export class ProductService {
       map(response => response._embedded.products))
   }
 
+  searchProductsPaginate(thePage: number,
+                         thePageSize: number,
+                         theKeyword: string): Observable<GetResponseProducts>{
+
+    const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+      +`&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+
   getProduct(theProductID: number): Observable<Product> {
     //build url based on product id
     const productUrl= `${this.baseUrl}/${theProductID}`;
