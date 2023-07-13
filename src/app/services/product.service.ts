0a8509de1752995cfa1cd10419ee98fae4038541
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-13 22:17:50 +0200

    added page to GetResponseProducts interface and created new method for pagination

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -42,11 +42,30 @@ export class ProductService {
 
     return this.httpClient.get<Product>(productUrl);
   }
+
+  //pagination
+  gerProductListPaginate(thePage: number,
+                         thePageSize: number,
+                         theCategoryId:number): Observable<GetResponseProducts>{
+
+    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+      +`&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
 }
+
+
 
 interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  },
+  page:{
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 interface GetResponseProductCategory{
