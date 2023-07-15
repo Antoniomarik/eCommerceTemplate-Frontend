c1924f1891c48165c7f67d02a72632e8982238a6
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-15 10:36:05 +0200

    injected cartService into cart details component and subscribe totalPrice total quantity and handled cartItems

--- src/app/componencts/cart-details/cart-details.component.ts
+++ src/app/componencts/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-cart-details',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-details.component.css']
 })
 export class CartDetailsComponent {
+  cartItem: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {
+  }
+
+  listCartDetails(){
+    //get a handle to the cart items
+    this.cartItem = this.cartService.cartItems;
+
+    //subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+    //subscribe to the cart total quantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+
+    //compute cart total price and quantity
+    this.cartService.computeCartTotals()
+  }
 }
