e2a8089be6bd56848fa425ec4b6055c4999d69e4
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-15 11:33:58 +0200

    minor  updated, displaying msg if cart is empty

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -42,15 +42,6 @@ export class CartService {
 
     //compute cart total price and total quantity
     this.computeCartTotals();
-
-    console.log('Contents of the cart');
-    for (let tempCartItem of this.cartItems) {
-      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
-      console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
-    }
-
-
-    console.log('----');
   }
 
    computeCartTotals() {
