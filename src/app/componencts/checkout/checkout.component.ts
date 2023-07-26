4466433010c894c310a261ae4fa578923aad9e1b
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-26 21:09:04 +0200

    added extra part of html to review order price, quantity and shipping (functionality not done yet)

--- src/app/componencts/checkout/checkout.component.ts
+++ src/app/componencts/checkout/checkout.component.ts
@@ -9,6 +9,9 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class CheckoutComponent implements OnInit{
   checkoutFormGroup: FormGroup;
 
+
+  totalPrice = 0;
+  totalQuantity = 0;
   constructor(private formBuilder: FormBuilder) {
   }
 
